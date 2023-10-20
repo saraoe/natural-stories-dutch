5babd4f4aa5eadf9fdea89035b0983b9b230b2b9
saraoe <sara.m.oestergaard@gmail.com>
2023-10-20 14:57:53 +0200

    filename update

--- src/docx_to_txt.py
+++ src/docx_to_txt.py
@@ -21,7 +21,7 @@ for i, docx_file in enumerate(glob(stories_path)):
     if "docx" not in docx_file:
         os.rename(docx_file, f"{docx_file}.docx")
         docx_file = f"{docx_file}.docx"
-    file_name = docx_file[8:-6]
+    file_name = docx_file[8:-5]
     file_name = re.sub("\s", "_", file_name)
     file_name = re.sub("[^_]\W", "", file_name)
     print(f"{i}: {file_name}")
