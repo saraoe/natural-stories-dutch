c0b496c528204f4a1990bc46db09fb7d4e1bef7b
saraoe <sara.m.oestergaard@gmail.com>
2023-11-07 15:26:49 +0100

    add qs

--- experiment/src/self-paced_reading.py
+++ experiment/src/self-paced_reading.py
@@ -6,13 +6,24 @@ from psychopy import visual, core
 import os
 import pandas as pd
 from read_txt import read_text
-from show_stim import show_fixation, show_text, show_word, show_blackscreen
+from show_stim import (
+    show_fixation,
+    show_text,
+    show_word,
+    show_blackscreen,
+    show_questions,
+)
 
 
 def experiment(paths: dict, fixation_time: int, blackscreen_time: int):
     stopwatch = core.Clock()
-    df = pd.DataFrame()
-    questions_df = pd.read_csv(paths["questions"])
+
+    # empty dfs
+    rt_df = pd.DataFrame()
+    responses = []
+
+    # questions
+    questions_df = pd.read_excel(paths["questions"])
 
     # GUI information
 
@@ -37,7 +48,7 @@ def experiment(paths: dict, fixation_time: int, blackscreen_time: int):
             words = paragraph.split(" ")
             for word in words:
                 rt = show_word(word, text_stim, win, stopwatch)
-                df = df.append(
+                rt_df = rt_df.append(
                     {"reation_time": rt, "story": story_name, "word": word},
                     ignore_index=True,
                 )
@@ -45,7 +56,10 @@ def experiment(paths: dict, fixation_time: int, blackscreen_time: int):
                 show_blackscreen(win, sec=blackscreen_time)
 
         # questions
+        document_id = 1  # fix this!
         qs = questions_df[questions_df["document_id"] == document_id]
+        tmp_responses = show_questions(qs, text_stim, win)
+        responses += tmp_responses
 
     # show ending
     for end in read_text(paths["end"]):
@@ -54,7 +68,11 @@ def experiment(paths: dict, fixation_time: int, blackscreen_time: int):
     # saving data
     if not os.path.exists(paths["out_data"]):
         os.makedirs(paths["out_data"])
-    df.to_csv(os.path.join(paths["out_data"], "data.csv"))  # change path!
+    rt_df.to_csv(os.path.join(paths["out_data"], "rt.csv"))  # change path!
+    responses_df = pd.DataFrame(responses, index=range(len(responses)))
+    responses_df.to_csv(
+        os.path.join(paths["out_data"], "responses.csv")
+    )  # change path!
 
 
 if __name__ == "__main__":
@@ -62,8 +80,8 @@ if __name__ == "__main__":
     paths = {
         "instruction": os.path.join("..", "instructions", "instruction*.txt"),
         "end": os.path.join("..", "instructions", "end.txt"),
-        "stories": os.path.join("..", "stories", "*"),
-        "questions": os.path.join("..", "questions.csv"),
+        "stories": os.path.join("..", "..", "texts", "*"),
+        "questions": os.path.join("..", "questions.xlsx"),
         "out_data": os.path.join("..", "data"),
     }
 
--- experiment/src/show_stim.py
+++ experiment/src/show_stim.py
@@ -2,6 +2,8 @@
 functions for showing stimuli in psychopy scripts
 """
 from psychopy import core, event
+import pandas as pd
+from random import shuffle
 
 
 def show_fixation(stim, win, sec):
@@ -15,14 +17,15 @@ def show_blackscreen(win, sec):
     core.wait(sec)
 
 
-def show_text(word: str, text_stim, win):
+def show_text(word: str, text_stim, win, possible_keys=None):
     text_stim.text = word
     text_stim.draw()
     win.flip()
-    key = event.waitKeys()[0]
+    key = event.waitKeys(keyList=possible_keys)[0]
     if key in ["escape", "q"]:
         win.close()
         core.quit()
+    return key
 
 
 def show_word(word: str, text_stim, win, stopwatch):
@@ -36,3 +39,39 @@ def show_word(word: str, text_stim, win, stopwatch):
         win.close()
         core.quit()
     return rt
+
+
+def show_question(question: str, answers: dict, text_stim, win):
+    answers_list = list(answers.keys())
+    shuffle(answers_list)
+    q_str = f"{question} \n\n"
+    for i, answer in enumerate(answers_list, start=1):
+        q_str += f"{i}: {answer} \n"
+
+    key = show_text(
+        q_str, text_stim, win, possible_keys=["escape", "q", "1", "2", "3", "4"]
+    )
+    response_key = int(key)
+    response_letter = answers[answers_list[response_key - 1]]
+    return response_letter
+
+
+def show_questions(questions_df: pd.DataFrame, text_stim, win):
+    response_list = []
+
+    questions_df = questions_df.sample(frac=1)  # shuffle questions
+    for index, row in questions_df.iterrows():
+        ans_cols = ["a-correct", "b", "c", "d"]
+        answers = {row[col]: col for col in ans_cols}
+        response = show_question(row["Question"], answers, text_stim, win)
+        correct = 1 if response == "a-correct" else 0
+
+        response_list.append(
+            {
+                "response": response,
+                "correct": correct,
+                "document_id": row["document_id"],
+                "question_id": row["question_id"],
+            }
+        )
+    return response_list
