e65806856859c7c7987b513c050bb07ef3d265c3
saraoe <sara.m.oestergaard@gmail.com>
2024-11-27 16:08:58 +0100

    summarize erp_df

--- paper/src/util.r
+++ paper/src/util.r
@@ -12,13 +12,38 @@ filter_erps <- function(erps, reading_cond) {
         filter(reading_type == reading_cond) |>
         filter(!is.na(lp_quantile)) |>
         filter(!(document_id %in% c(11, 12))) |> # exclude practice texts
-        group_by(.time, .key, lp_quantile, participant_number) |>
+        group_by(.time, .key, lp_quantile) |>
         summarize(
             ".value" = mean(.value),
             ".value_action_words" = mean(.value_action_words)
         ) |>
         mutate("reading_type" = reading_cond)
     return(filt_erps)
+}
+
+update_erp_df <- function(cur_df, new_df) {
+    new_df <- new_df |>
+        rename(
+            "value_new" = .value,
+            "value_aw_new" = .value_action_words
+        ) |>
+        mutate("n_subjects_new" = 1)
+
+    updated_df <- cur_df |>
+        left_join(new_df) |>
+        rowwise() |>
+        mutate(
+            ".value" = sum(.value, value_new, na.rm = TRUE),
+            ".value_action_words" = sum(
+                .value_action_words,
+                value_aw_new,
+                na.rm = TRUE
+            ),
+            "n_subjects" = sum(n_subjects, n_subjects_new, na.rm = TRUE)
+        ) |>
+        select(-value_new, -value_aw_new, -n_subjects_new)
+
+    return(updated_df)
 }
 
 read_filt_erps <- function(erp_folder, filename, reject_df, overwrite = FALSE) {
@@ -31,6 +56,8 @@ read_filt_erps <- function(erp_folder, filename, reject_df, overwrite = FALSE) {
         )
 
         for (erp_file in erp_files) {
+            print(erp_file)
+
             erps <- read.csv(erp_file) |>
                 select(-X) |>
                 left_join(reject_df,
@@ -44,11 +71,19 @@ read_filt_erps <- function(erp_folder, filename, reject_df, overwrite = FALSE) {
 
             # save for return
             if (exists("erp_df_")) {
-                erp_df_ <- rbind(erp_df_, tmp_erp)
+                erp_df_ <- update_erp_df(erp_df_, tmp_erp)
             } else {
-                erp_df_ <- tmp_erp
+                erp_df_ <- tmp_erp |>
+                    mutate("n_subjects" = 1)
             }
         }
+        # calculate mean over all participants
+        erp_df_ <- erp_df_ |>
+            mutate(
+                ".value" = .value / n_subjects,
+                ".value_action_words" = .value_action_words / n_subjects
+            )
+
         # save global erp file
         write.csv(erp_df_, filename)
     } else {
