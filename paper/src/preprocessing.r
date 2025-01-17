71cc9d7393bdc8dd6b618885ed0c3cf30fadafcc
saraoe <sara.m.oestergaard@gmail.com>
2025-01-17 15:25:31 +0100

    make zero_freq boolean

--- paper/src/preprocessing.r
+++ paper/src/preprocessing.r
@@ -20,6 +20,7 @@ sterp_filename <- "data/sterp.csv"
 # files
 eeg_files <- list.files("data/spr/", full.names = TRUE, pattern = "df$")
 stim <- read.csv("data/stim.csv") |>
+    mutate("zero_freq" = as.logical(zero_freq)) |>
     mutate(
         lp_quantile = case_when(
             lp >= quantile(lp, na.rm = TRUE)[4] ~ "high_lp",
